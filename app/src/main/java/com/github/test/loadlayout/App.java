057366cc1ef9a162eb7c90087791f2152c6ab499
若云 <zyhdvlp@gmail.com>
2018-11-13 10:21:22 +0800

    升级 gradle

--- app/src/main/java/com/github/test/loadlayout/App.java
+++ app/src/main/java/com/github/test/loadlayout/App.java
@@ -1,6 +1,8 @@
 package com.github.test.loadlayout;
 
+import android.app.Activity;
 import android.app.Application;
+import android.os.Bundle;
 
 /**
  * Created by ruoyun on 2018/7/31.
@@ -13,6 +15,44 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+
+            }
+        });
+
     }
 
 
